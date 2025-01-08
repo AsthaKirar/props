13c0a8fff411db7820d53c693195e0ace59f8322
AsthaKirar <asthap602@gmail.com>
2025-01-08 20:57:55 +0530

    commit

--- custom hook/src/App.jsx
+++ custom hook/src/App.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <><div className='bg-orange-600 text-center'>currency App</div></>
   )
 }
 
 export default App
+
+
+
+
+
+
+//component ko mount karne ke liye useeffect ka use karte hai
